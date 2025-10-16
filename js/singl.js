f7488f12dfa9a6d22212d7a6808b67a1ae12ea99
Alisher20060109 <gameaxiis@gmail.com>
2025-10-16 12:40:39 +0500

    making electro becycle

--- js/singl.js
+++ js/singl.js
@@ -11,12 +11,14 @@ singlProduct.map((el) => {
                             <div class="flex items-center gap-4 justify-between  relative">
                                 <div class="flex flex-col gap-4">
                                    ${el.images.map((img) => {
-                                    return ` <img class="max-w-[100px] w-full" src="${img}" alt="">
+                                     return ` <img class="max-w-[100px] w-full" src="${img}" alt="">
                                     `;
                                    })}
                                 </div>
                                 <div class="relative">
-                                    <img class="max-w-[500px] w-full" src="${el.images[0]}" alt="">
+                                    <img class="max-w-[500px] w-full" src="${
+                                      el.images[0]
+                                    }" alt="">
                                     <h1
                                         class="absolute top-3 right-3 text-[18px] bg-[#FF6633] px-3 py-1 text-white rounded-lg">
                                         ${el.discount}%
@@ -27,11 +29,14 @@ singlProduct.map((el) => {
                             <div>
                                 <div class="flex items-center justify-between">
                                     <div>
-                                        <p>$200</p>
+                                        <p>$${el.price}</p>
                                         <p class="text-[12px] text-[#8F8F8F]">Обычная цена</p>
                                     </div>
                                     <div>
-                                        <p>$100</p>
+                                        <p>$${
+                                          el.price -
+                                          (el.price * el.discount) / 100
+                                        }</p>
                                         <p class="text-[12px] text-[#8F8F8F]">С картой Северяночки</p>
                                     </div>
 
